a314456bd9f1bd75e9ac4d532ae2b027578f0ffc
Alan George <ageorge@alan-george.com>
2017-09-18 14:14:40 -0700

    rxjava sesssion timeout

--- test/rxjava_play/RxJavaPlay.kt
+++ test/rxjava_play/RxJavaPlay.kt
@@ -6,9 +6,11 @@ import org.junit.Test
 import rx.Observable.*
 import rx.observers.TestSubscriber
 import rx.schedulers.Schedulers
+import rx.subjects.PublishSubject
 import java.util.*
 import java.util.concurrent.Executors.newFixedThreadPool
 import java.util.concurrent.ThreadFactory
+import java.util.concurrent.TimeUnit
 
 
 class RxJavaPlay {
@@ -79,6 +81,79 @@ class RxJavaPlay {
         ts.awaitTerminalEvent()
     }
 
+    @Test
+    fun rxjava_session_timeout() {
+        val signOnSuccess = "signOnSuccess"
+        val timeoutTimeUnit = TimeUnit.MILLISECONDS
+        val warningTime = 2500L
+        val sessionTimeout = 3000L
+
+        val signOnEvents = PublishSubject.create<String>()
+        val networkEvents = PublishSubject.create<Int>()
+
+        val eventGeneratorObs = create<Unit> { sub ->
+            Thread.sleep(1000)
+            networkEvents.onNext(1)
+            signOnEvents.onNext("signOnInit")
+            Thread.sleep(250)
+            signOnEvents.onNext(signOnSuccess)
+            Thread.sleep(500)
+            signOnEvents.onNext("someOtherSignOnEvent")
+            networkEvents.onNext(2)
+            Thread.sleep(500)
+            networkEvents.onNext(3)
+            networkEvents.onNext(4)
+            Thread.sleep(1000)
+            networkEvents.onNext(5)
+        }
+
+        val sessionStartObs = signOnEvents
+                .filter { it?.equals(signOnSuccess) }
+                .map { SessionStart(System.currentTimeMillis()) }
+                .share()
+
+        val sessionTimeoutWarningObs = sessionStartObs
+                .switchMap { start ->
+                    networkEvents
+                            .doOnNext { printWithThreadInfo("in session network event $it: session length ${System.currentTimeMillis() - start.time}") }
+                            .debounce(warningTime, timeoutTimeUnit, schedulerB)
+                            .map { SessionTimeoutWarning(System.currentTimeMillis() - start.time) }
+                }
+                .share()
+
+        val sessionTimeoutObs = sessionStartObs
+                .switchMap { start ->
+                    networkEvents
+                            .doOnNext { printWithThreadInfo("in session network event $it: session length ${System.currentTimeMillis() - start.time}") }
+                            .debounce(sessionTimeout, timeoutTimeUnit, schedulerC)
+                            .map { SessionTimeout(System.currentTimeMillis() - start.time) }
+                }
+
+        val ts1 = TestSubscriber<SessionTimeout>()
+
+        eventGeneratorObs
+                .subscribeOn(schedulerA)
+                .subscribe()
+
+        sessionTimeoutWarningObs
+                .doOnNext { printWithThreadInfo("sessionTimeoutWarning: $it") }
+                .subscribe()
+
+        sessionTimeoutWarningObs
+                .first()
+                .doOnNext {
+                    printWithThreadInfo("extending session: $it")
+                    networkEvents.onNext(6)
+                }
+                .subscribe()
+
+        sessionTimeoutObs
+                .first()
+                .doOnNext { printWithThreadInfo("sessionTimeout: $it") }
+                .subscribe(ts1)
+
+        ts1.awaitTerminalEvent()
+    }
 
     var startTime = Date().time
 
@@ -86,3 +161,7 @@ class RxJavaPlay {
         println("${Date().time - startTime}: value $i on thread ${Thread.currentThread().name}")
     }
 }
+
+data class SessionStart(val time: Long)
+data class SessionTimeout(val sessionLength: Long)
+data class SessionTimeoutWarning(val sessionLength: Long)
